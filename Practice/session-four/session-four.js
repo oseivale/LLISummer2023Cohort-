017fb3a8ed99b957bf64ec37835adf0063ab2b7a
Valerie Wireko Osei <valeriew@SML391TO.local>
2023-07-20 19:41:55 -0400

    adding previous work

--- Practice/session-four/session-four.js
+++ Practice/session-four/session-four.js
@@ -16,19 +16,67 @@ function greeting(){
    `;
 }
 
+const cards = [
+   {
+      title: 'My Card Title #1',
+      className: 'card',
+      subtitle: 'My Card subtitle #1',
+      imageUrl: "https://img.freepik.com/free-vector/summer-lettering-with-photo_23-2148523751.jpg?size=626&ext=jpg"
+   },
+   {
+      title: 'My Card Title #2',
+      className: 'card',
+      subtitle: 'My Card subtitle #2',
+      imageUrl: "https://img.freepik.com/free-vector/summer-lettering-with-photo_23-2148523751.jpg?size=626&ext=jpg"
+   },
+   {
+      title: 'My Card Title #3',
+      className: 'card',
+      subtitle: 'My Card subtitle #3',
+      imageUrl: "https://img.freepik.com/free-vector/summer-lettering-with-photo_23-2148523751.jpg?size=626&ext=jpg"
+   }
+]
+
 function generateCard(){
-   var card = document.createElement("div")
-   var title = document.createElement("h1")
-   var img = document.createElement("img")
+   for(let card of cards){
+      var cardElement = document.createElement("div")
+      var title = document.createElement("h1")
+      var subtitle = document.createElement("h3")
+      var img = document.createElement("img")
 
-   title.innerText = "My Card Title"
-   img.src = "https://img.freepik.com/free-vector/summer-lettering-with-photo_23-2148523751.jpg?size=626&ext=jpg"
+      title.innerText = card.title
+      img.src = card.imageUrl
+      subtitle.innerText = card.subtitle
+      cardElement.classList.add(card.className)
 
-   myDiv.appendChild(card)
-   
-   card.appendChild(title)
-   card.appendChild(img)
-  
+      myDiv.appendChild(cardElement)
+      cardElement.appendChild(title)
+      cardElement.appendChild(subtitle)
+      cardElement.appendChild(img)
+   }
 }
 
 button.addEventListener("click", generateCard)
+
+const myArray = [
+   {
+      name: 'Bananas',
+      price: 1.5
+   },
+   {
+      name: 'Cereal',
+      price: 6.0
+   },
+   {
+      name: 'Milk',
+      price: 5.5
+   },
+   {
+      name: 'Cookies',
+      price: 1.8
+   }
+]
+
+for (let item of myArray){
+   console.log("I went to the store and I bought " + item.name)
+}
--- Project/project.js
+++ Project/project.js
@@ -1,1 +1,55 @@
+const gridContainer = document.querySelector(".grid-container");
+let cards = [];
+let firstCard, secondCard;
+let lockBoard = false;
+let score = 0;
 
+document.querySelector(".score").textContent = score;
+
+function generateCards(){
+    for(let card of cards){
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("card");
+        cardElement.setAttribute("data-name", card.name)
+        cardElement.innerHTML = `
+            <div class="front-image-container">
+                <img src=${card.image} />
+            </div>
+            <div class="back"></div>
+        `; 
+        gridContainer.appendChild(cardElement);
+        cardElement.addEventListener("click", flipCard);
+    }
+}
+
+function flipCard(){
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
+    this.classList.add("flipped")
+
+    if(!firstCard){
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    score++;
+    document.querySelector(".score").textContent = score;
+    lockBoard = true;
+
+    checkForMatch()
+}
+
+function checkForMatch(){
+    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards(){
+    firstCard.removeEventListener("click", flipCard)
+    secondCard.removeEventListener("click", flipCard)
+
+    resetBoard()
+}
